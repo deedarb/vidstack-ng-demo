0b10e652a6ef3ca309c0c6b7757d1920cc62db28
Didar Burmaganov <deedarb@gmail.com>
2024-01-08 15:11:34 +0600

    angular vidstack demo

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'vidstack/player/styles/default/theme.css';
+import 'vidstack/player/styles/default/layouts/video.css';
+import 'vidstack/player';
+import 'vidstack/player/layouts';
+import 'vidstack/player/ui';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
